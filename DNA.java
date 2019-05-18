2c1f6a2af7848c085d32b81d784667275891ca70
OPzCatchMee <cmnguyen999@gmail.com>
2019-05-18 14:07:59 -0500

    first commit

---
+++ DNA.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+
+/*
+  DNA in binary
+    00 -> A
+    01 -> T
+    10 -> G
+    11 -> C
+*/
+
+public class DNA {
+
+  public static void main() {
+    Scanner input = new Scanner(System.in);
+
+
+
+
+  }
+
+  public static void encode() {
+
+  }
+
+  public static void options() {
+    System.out.println();
+  }
+
+}
